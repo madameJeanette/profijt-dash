abbd0331fb8342172677e9414de479044fdc91ea
Danielle <danielle_meerburg@live.nl>
2021-06-03 14:35:57 +0200

    homebasestyling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 import React from 'react'
-
+import image from "../bgImg.jpg"
 export default function Home() {
-  return <h1>Home!</h1>
+  return (
+  <main>
+    <img src={image} alt="colorfull mountains" className="absolute object-cover w-full h-full"/>
+    <section className="relative flex justify-left min-h-screen  pt-12 lg:pt-164 px-8">
+      <h1 className="text-4xl text-gray-800 font-bold leading-none lg:leading-snug ">Welkom terug!</h1>
+    </section>
+  </main>
+    )
 }
