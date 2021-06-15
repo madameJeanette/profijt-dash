0521a448681b70492bd3fd3013c5f4f933471271
Danielle <danielle_meerburg@live.nl>
2021-06-15 15:13:19 +0200

    Lesmateriaal pagina

--- src/components/Books.js
+++ src/components/Books.js
@@ -8,16 +8,17 @@ import image5 from '../img/yBook.jpg'
 export default function Books() {
   return (
     
-  <div class="grid grid-cols-3 gap-4">
+  <div className="grid grid-cols-3 gap-4">
+
     <NavLink to="/books/Gr">    
 
-      <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
-       <img class="mx-auto" src={image1} alt="green book"/>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-center">Groen</div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
+       <img className="mx-auto" src={image1} alt="green book"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">Groen</div>
 
         </div>
-        <div class="px-6 py-4">
+        <div className="px-6 py-4">
         </div>
       </div>
     </NavLink>
@@ -25,56 +26,56 @@ export default function Books() {
   
     <NavLink to="/books/wi">    
 
-      <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
-       <img class="mx-auto" src={image2} alt="green book"/>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-center">Rekenen</div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
+       <img className="mx-auto" src={image2} alt="green book"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">Rekenen</div>
 
         </div>
-        <div class="px-6 py-4">
+        <div className="px-6 py-4">
         </div>
       </div>
     </NavLink>
 
     <NavLink to="/books/nl">    
 
-      <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
-       <img class="mx-auto" src={image3} alt="green book"/>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-center">Nederlands</div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
+       <img className="mx-auto" src={image3} alt="green book"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">Nederlands</div>
 
         </div>
-        <div class="px-6 py-4">
+        <div className="px-6 py-4">
         </div>
       </div>
     </NavLink>
 
     <NavLink to="/books/brgr">    
 
-      <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
-       <img class="mx-auto" src={image4} alt="green book"/>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-center">Burgerschap</div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
+       <img className="mx-auto" src={image4} alt="green book"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">Burgerschap</div>
 
         </div>
-        <div class="px-6 py-4">
+        <div className="px-6 py-4">
         </div>
       </div>
     </NavLink>
 
     <NavLink to="/books/ict">    
 
-      <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
-       <img class="mx-auto" src={image5} alt="puple book"/>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-center">ICT</div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 my-2">
+       <img className="mx-auto" src={image5} alt="purple book"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">ICT</div>
 
         </div>
-        <div class="px-6 py-4">
+        <div className="px-6 py-4">
         </div>
       </div>
     </NavLink>
-
+ 
   </div>
   )
   //nav opties Nederlandse  Rekenen  Groen Burgerschap  ICT"
